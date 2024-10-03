214f13660c36bae575fcb0b5f27d683f028c003f
suhraby <suhrab22@gmail.com>
2024-10-03 14:03:11 +0500

    add 09-template-condition

---
+++ courses/09-template-condition/App.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="text-center p-8">
+		<div class="py-4">
+			<h3 class="text-2xl font-semibold">If - else</h3>
+			<div v-if="isFirst">Welcome to home page</div>
+			<div v-else>Welcome again</div>
+			<button class="border border-gray-400 rounded-md py-2 px-4" @click="isFirst = !isFirst">Click me</button>
+		</div>
+
+		<div>
+			<h3 class="text-2xl font-semibold">If - elseif - else</h3>
+			<div v-if="text == 'a'">You typed a</div>
+			<div v-else-if="text == 'b'">You typed b</div>
+			<div v-else>You didn't type a nor b</div>
+			<button class="border border-gray-400 rounded-md py-2 px-4" @click="doIt">Click me</button>
+		</div>
+	</div>
+</template>
+
+<script lang="ts" setup>
+import { Ref, ref } from 'vue';
+
+const isFirst: Ref<boolean> = ref(true);
+const text: Ref<string> = ref('');
+
+const doIt = () => {
+	text.value = prompt('Type the a or b')!;
+};
+</script>
